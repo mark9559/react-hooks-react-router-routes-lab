db4fc3c18f1e0e82298ddb360ec5037e92cb3bde
Mark Mwangi <mwangimark57@gmail.com>
2023-11-03 02:29:28 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Movies from './Movies';
+import Directors from './Directors';
+import Actors from './Actors';
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/movies">
+            <Movies />
+          </Route>
+          <Route exact path="/directors">
+            <Directors />
+          </Route>
+          <Route exact path="/actors">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
